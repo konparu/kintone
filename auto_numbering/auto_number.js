986e285bea2c9145ae3eed4dfbf31c477ee97af7
konparu <konparu@r3it.com>
2015-01-14 09:18:04 +0900

    simplified

--- auto_numbering/auto_number.js
+++ auto_numbering/auto_number.js
@@ -11,23 +11,7 @@
         for( var i = 0; i < count; i++ ){
             record.receipts.value[i].value.no.value = i + 1;
         }
-        
-        // "レコード名" に 作成者 & "_" & 対象年月 の値を入れる
-        // フィールドの自動計算ではうまくできなかったためJSで実施する
-        record.レコード名.value = record.対象年月.value + "_" + record.作成者.value.name;
-        
+
         return event;
-    });
-    
-    // レコード名は、表示する必要がないため非表示にする
-    var showEvents = [
-        'app.record.detail.show',
-        'app.record.create.show',
-        'app.record.edit.show',
-    ];
-    
-    kintone.events.on(showEvents, function(event){
-       var record = event.record;
-       kintone.app.record.setFieldShown('レコード名', false); 
     });
 })();
